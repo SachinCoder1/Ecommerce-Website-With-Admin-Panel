23a93f7aacc8062bfdea40ff7175ea727b681144
SachinCoder1 <sachinhlo232@gmail.com>
2022-07-05 14:04:52 +0530

    admin middleware

---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,16 @@
+const Users = require('../models/userSchema')
+
+const adminAuth = async (req, res, next) => {
+    try {
+        const user = await Users.findOne({_id: req.user.id});
+        if(user.role === 0) return res.status(400).json({msg: "Access denied"})
+      
+        //  If Admin
+        next();
+
+    } catch (error) {
+        return res.status(500).json({msg: err.message})
+    }
+}
+
+module.exports = adminAuth;
